aa727e2b3202d3002c8d0723226a032083a5e2c3
Andrey.Skripay <lordktan@gmail.com>
2023-12-05 09:46:29 +0500

    Задача 2.3 доработка 4

--- src/main/java/com/example/Homework233/CalculatorController.java
+++ src/main/java/com/example/Homework233/CalculatorController.java
@@ -1,14 +1,11 @@
 package com.example.Homework233;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
 @RestController
-@RequestMapping("/calculator")
 public class CalculatorController {
 
     private final CalculatorService calculatorService;
@@ -17,34 +14,35 @@ public class CalculatorController {
         this.calculatorService = calculatorService;
     }
 
-    @GetMapping
+    @GetMapping("/calculator")
     public String welcome() {
         return "Добро пожаловать в калькулятор";
     }
 
-    @GetMapping("/plus")
+    @GetMapping("/calculator/plus")
     public String add(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
         int result = calculatorService.add(num1, num2);
         return num1 + " + " + num2 + " = " + result;
     }
 
-    @GetMapping("/minus")
+    @GetMapping("/calculator/minus")
     public String subtract(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
         int result = calculatorService.subtract(num1, num2);
         return num1 + " - " + num2 + " = " + result;
     }
 
-    @GetMapping("/multiply")
+    @GetMapping("/calculator/multiply")
     public String multiply(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
         int result = calculatorService.multiply(num1, num2);
         return num1 + " * " + num2 + " = " + result;
     }
 
-    @GetMapping("/divide")
+    @GetMapping("/calculator/divide")
     public String divide(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
+        if (num2 == 0) {
+            return "Ошибка: деление на 0 недопустимо";
+        }
         int result = calculatorService.divide(num1, num2);
         return num1 + " / " + num2 + " = " + result;
     }
 }
-
-
