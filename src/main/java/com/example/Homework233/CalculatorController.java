4886e54d2c34699149eee1337db985de8dead63d
Andrey.Skripay <lordktan@gmail.com>
2023-12-05 09:34:56 +0500

    Задача 2.3 доработка 3

--- src/main/java/com/example/Homework233/CalculatorController.java
+++ src/main/java/com/example/Homework233/CalculatorController.java
@@ -1,45 +1,50 @@
 package com.example.Homework233;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/calculator")
 public class CalculatorController {
 
-    @Autowired
-    private CalculatorService calculatorService;
+    private final CalculatorService calculatorService;
 
-    @GetMapping("/calculator")
+    public CalculatorController(CalculatorService calculatorService) {
+        this.calculatorService = calculatorService;
+    }
+
+    @GetMapping
     public String welcome() {
         return "Добро пожаловать в калькулятор";
     }
 
-    @GetMapping("/calculator/plus")
+    @GetMapping("/plus")
     public String add(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
         int result = calculatorService.add(num1, num2);
         return num1 + " + " + num2 + " = " + result;
     }
 
-    @GetMapping("/calculator/minus")
+    @GetMapping("/minus")
     public String subtract(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
         int result = calculatorService.subtract(num1, num2);
         return num1 + " - " + num2 + " = " + result;
     }
 
-    @GetMapping("/calculator/multiply")
+    @GetMapping("/multiply")
     public String multiply(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
         int result = calculatorService.multiply(num1, num2);
         return num1 + " * " + num2 + " = " + result;
     }
 
-    @GetMapping("/calculator/divide")
+    @GetMapping("/divide")
     public String divide(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
-        try {
-            int result = calculatorService.divide(num1, num2);
-            return num1 + " / " + num2 + " = " + result;
-        } catch (IllegalArgumentException e) {
-            return "Error: " + e.getMessage();
-        }
+        int result = calculatorService.divide(num1, num2);
+        return num1 + " / " + num2 + " = " + result;
     }
 }
+
+
--- src/main/java/com/example/Homework233/CalculatorService.java
+++ src/main/java/com/example/Homework233/CalculatorService.java
@@ -1,5 +1,7 @@
 package com.example.Homework233;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CalculatorService {
     public static int add(int num1, int num2) {
         return num1 + num2;
