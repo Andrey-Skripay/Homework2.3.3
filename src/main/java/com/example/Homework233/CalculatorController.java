759e0e977608c75c42667f9b9aded73e5b5d1625
Andrey.Skripay <lordktan@gmail.com>
2023-12-05 09:03:45 +0500

    Задача 2.3 доработка 2

--- src/main/java/com/example/Homework233/CalculatorController.java
+++ src/main/java/com/example/Homework233/CalculatorController.java
@@ -1,9 +1,15 @@
 package com.example.Homework233;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class CalculatorController {
+
+    @Autowired
+    private CalculatorService calculatorService;
+
     @GetMapping("/calculator")
     public String welcome() {
         return "Добро пожаловать в калькулятор";
@@ -11,28 +17,29 @@ public class CalculatorController {
 
     @GetMapping("/calculator/plus")
     public String add(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
-        int result = num1 + num2;
+        int result = calculatorService.add(num1, num2);
         return num1 + " + " + num2 + " = " + result;
     }
 
     @GetMapping("/calculator/minus")
     public String subtract(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
-        int result = num1 - num2;
+        int result = calculatorService.subtract(num1, num2);
         return num1 + " - " + num2 + " = " + result;
     }
 
     @GetMapping("/calculator/multiply")
     public String multiply(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
-        int result = num1 * num2;
+        int result = calculatorService.multiply(num1, num2);
         return num1 + " * " + num2 + " = " + result;
     }
 
     @GetMapping("/calculator/divide")
     public String divide(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
-        if (num2 == 0) {
-            return "Ошибка: деление на 0 недопустимо";
+        try {
+            int result = calculatorService.divide(num1, num2);
+            return num1 + " / " + num2 + " = " + result;
+        } catch (IllegalArgumentException e) {
+            return "Error: " + e.getMessage();
         }
-        int result = num1 / num2;
-        return num1 + " / " + num2 + " = " + result;
     }
 }
